cc36b22641c38195ad7bc810d1de0e7a17c3c6d2
Scott Durant <scott.durant.16@cnu.edu>
2018-07-04 11:13:35 -0400

    made a calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+
+    public Calculator() {
+        
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Welcome to the Fantasy Supercross Calculator!");
+    }
+
+}
