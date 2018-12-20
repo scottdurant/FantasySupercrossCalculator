57ea45dcf54f64bb41eb2f649449c220b198f517
Scott Durant <scott.durant.16@cnu.edu>
2018-12-20 18:06:08 -0500

    new method and tests for that method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 
+/*
+ * TODO: Make sure no points are added if the wildcard places in the top 5
+ * Get user input/make menu system
+ */
+
+
 public class Calculator {
     
     private int score;
@@ -9,14 +15,10 @@ public class Calculator {
     }
 
     public int calculateScore(ArrayList<String> results, ArrayList<String> prediction) {
-        // check for exact guesses
-        for (int i = 0; i < results.size(); i++) {
+        for (int i = 0; i < 5; i++) {
             String resultName = results.get(i);
             String predictionName = prediction.get(i);
-
-            
             // if results contains predictionName, check if exact match. 
-            
             if (results.contains(predictionName)){
                 // if the result at i equals the prediction at i, the user has an exact
                 // guess, otherwise, it was not an exact guess, so add 5
@@ -36,35 +38,36 @@ public class Calculator {
                     if (i == 4) {
                         score += 18;
                     }
-                } else{
+                } else {
                     score += 5;
                 }
             }
             
-            /*
-            if (resultName.equals(predictionName)) {
-                // @TODO: make an enumerated class for the places compared to
-                // points??
-                if (i == 0 || i == 5) {
-                    score += 26;
-                }
-                if (i == 1) { 
-                    score += 23;
-                }
-                if (i == 2) {
-                    score += 21;
-                }
-                if (i == 3) {
-                    score += 19;
-                }
-                if (i == 4) {
-                    score += 18;
-                }
-            }
-            */
         }
-
+        
+        // check for wild card
+        
+        if(results.get(results.size()-1).equals(prediction.get(prediction.size()-1))){
+            score += 26;
+        }
+        
         return score;
+    }
+    
+    /*
+     * Checks if both the results and prediction array have a length of 6 ( top five
+     *  riders + wild card)
+     */
+    public boolean checkArrays(ArrayList<String> prediction, ArrayList<String> result){
+        if(prediction.size() != 6){
+            return false;
+        }
+        
+        if(result.size() != 6){
+            return false;
+        }
+        
+        return true;
     }
 
     public static void main(String[] args) {
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -43,8 +43,9 @@ public class CalculatorTest {
         assertEquals(133, score);
     }
 
-    // Note: these do not test if the user picks a rider who was in the top five, but the position was off
-    
+    // Note: these do not test if the user picks a rider who was in the top
+    // five, but the position was off
+
     @Test
     public void testJustFirstPlaceCorrect() {
         ArrayList<String> results = makeList("a", "b", "c", "d", "e", "f");
@@ -80,7 +81,7 @@ public class CalculatorTest {
 
         assertEquals(19, score);
     }
-    
+
     @Test
     public void testJustFifthPlaceCorrect() {
         ArrayList<String> results = makeList("a", "b", "c", "d", "e", "f");
@@ -89,7 +90,7 @@ public class CalculatorTest {
 
         assertEquals(18, score);
     }
-    
+
     @Test
     public void testJustWildcardCorrect() {
         ArrayList<String> results = makeList("a", "b", "c", "d", "e", "f");
@@ -98,24 +99,94 @@ public class CalculatorTest {
 
         assertEquals(26, score);
     }
-    
+
     @Test
-    public void testFirstAndSecondPickedInWrongOrder(){
+    public void testFirstAndSecondPickedInWrongOrder() {
         ArrayList<String> results = makeList("a", "b", "c", "d", "e", "f");
         ArrayList<String> prediction = makeList("b", "a", "i", "j", "k", "l");
         int score = makeCalculations(results, prediction);
 
         assertEquals(10, score);
     }
-    
+
     @Test
-    public void testFirstThirdAndFifthPickedInWrongOrder(){
-        ArrayList<String> results = makeList("Tomac", "Musquin", "Baggett", "Craig", "Anderson", "Bloss");
-        ArrayList<String> prediction = makeList("Baggett", "Tomac", "Anderson", "Wrong", "Wrong", "Wrong");
+    public void testFirstThirdAndFifthPickedInWrongOrder() {
+        ArrayList<String> results = makeList("Tomac", "Musquin", "Baggett",
+                "Craig", "Anderson", "Bloss");
+        ArrayList<String> prediction = makeList("Baggett", "Tomac", "Anderson",
+                "Wrong", "Wrong", "Wrong");
         int score = makeCalculations(results, prediction);
 
         assertEquals(15, score);
     }
 
+    @Test
+    public void testFirstCorrectSecondWrongOrder() {
+        ArrayList<String> results = makeList("Tomac", "Musquin", "Baggett",
+                "Craig", "Anderson", "Bloss");
+        ArrayList<String> prediction = makeList("Tomac", "Wrong", "Wrong",
+                "Wrong", "Musquin", "Wrong");
+        int score = makeCalculations(results, prediction);
+
+        assertEquals(31, score);
+    }
+
+    @Test
+    public void testNoPointsGivenForWildCardInTopFive() {
+        ArrayList<String> results = makeList("Tomac", "Musquin", "Baggett",
+                "Craig", "Anderson", "Bloss");
+        ArrayList<String> prediction = makeList("Tomac", "Bloss", "Wrong",
+                "Wrong", "Wrong", "Wrong");
+        int score = makeCalculations(results, prediction);
+
+        assertEquals(26, score);
+    }
+
+    // Tests for checkArrays method //////////////////////////////////////////
+
+    @Test
+    public void testCheckArraysWithCorrectSizeArrays() {
+        ArrayList<String> results = makeList("Tomac", "Musquin", "Baggett",
+                "Craig", "Anderson", "Bloss");
+        ArrayList<String> prediction = makeList("Tomac", "Bloss", "Wrong",
+                "Wrong", "Wrong", "Wrong");
+        Calculator calc = new Calculator();
+
+        boolean result = calc.checkArrays(results, prediction);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testCheckArraysWithOneIncorrectSizeArray() {
+        ArrayList<String> results = new ArrayList<String>();
+        results.add("Tomac");
+        results.add("Musquin");
+        results.add("Baggett");
+        results.add("Craig");
+        results.add("Anderson");
+        ArrayList<String> prediction = makeList("Tomac", "Bloss", "Wrong",
+                "Wrong", "Wrong", "Wrong");
+        Calculator calc = new Calculator();
+
+        boolean result = calc.checkArrays(results, prediction);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testCheckArraysWithTwoIncorrectSizeArray() {
+        ArrayList<String> results = new ArrayList<String>();
+        results.add("Tomac");
+        results.add("Musquin");
+        results.add("Baggett");
+        results.add("Craig");
+        results.add("Anderson");
+        ArrayList<String> prediction = makeList("Tomac", "Bloss", "Wrong",
+                "Wrong", "Wrong", "Wrong");
+        prediction.add("someOtherGuy");
+        Calculator calc = new Calculator();
+
+        boolean result = calc.checkArrays(results, prediction);
+        assertEquals(false, result);
+    }
 
 }
