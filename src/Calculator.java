150749dff644c3c5342e7b3b0da03973ba0f785d
Scott Durant <scott.durant.16@cnu.edu>
2018-12-21 10:56:42 -0500

    did a lot of refactoring

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,70 +3,84 @@ import java.util.ArrayList;
 /*
  * TODO: Make sure no points are added if the wildcard places in the top 5
  * Get user input/make menu system
+ * Throw exception if arrays are incorrectly sized
  */
 
-
 public class Calculator {
-    
+
     private int score;
 
     public Calculator() {
         this.score = 0;
     }
 
-    public int calculateScore(ArrayList<String> results, ArrayList<String> prediction) {
+    public int calculateScore(ArrayList<String> results,
+            ArrayList<String> prediction) {
+        
+        if(!checkArrays(prediction, results)){
+            System.out.println("Incorrect sized arrays!");
+            return 0;
+        }
+        
+        // check wildcards
+        String wildCardResult = results.get(5);
+        String wildCardPrediction = prediction.get(5);
+
+        if (wildCardResult.equals(wildCardPrediction)) {
+            score += 26;
+        }
+
+        // remove wildcards so we don't have to worry about them any more
+
+        results.remove(5);
+        prediction.remove(5);
+
+        // check for exact matches (don't look at wildcard)
         for (int i = 0; i < 5; i++) {
-            String resultName = results.get(i);
-            String predictionName = prediction.get(i);
-            // if results contains predictionName, check if exact match. 
-            if (results.contains(predictionName)){
-                // if the result at i equals the prediction at i, the user has an exact
-                // guess, otherwise, it was not an exact guess, so add 5
-                if(resultName.equals(predictionName)){
-                    if (i == 0 || i == 5) {
-                        score += 26;
-                    }
-                    if (i == 1) { 
-                        score += 23;
-                    }
-                    if (i == 2) {
-                        score += 21;
-                    }
-                    if (i == 3) {
-                        score += 19;
-                    }
-                    if (i == 4) {
-                        score += 18;
-                    }
-                } else {
+            // if there's a match, check what position it is
+            if (results.get(i).equals(prediction.get(i))) {
+                if (i == 0) {
+                    score += 26;
+                }
+                if (i == 1) {
+                    score += 23;
+                }
+                if (i == 2) {
+                    score += 21;
+                }
+                if (i == 3) {
+                    score += 19;
+                }
+                if (i == 4) {
+                    score += 18;
+                }
+            }
+            else {
+                // if the rider was picked, but the position was incorrect, add
+                // 5
+                if (results.contains(prediction.get(i))) {
                     score += 5;
                 }
             }
-            
         }
-        
-        // check for wild card
-        
-        if(results.get(results.size()-1).equals(prediction.get(prediction.size()-1))){
-            score += 26;
-        }
-        
+
         return score;
     }
-    
+
     /*
-     * Checks if both the results and prediction array have a length of 6 ( top five
-     *  riders + wild card)
+     * Checks if both the results and prediction array have a length of 6 ( top
+     * five riders + wild card)
      */
-    public boolean checkArrays(ArrayList<String> prediction, ArrayList<String> result){
-        if(prediction.size() != 6){
+    public boolean checkArrays(ArrayList<String> prediction,
+            ArrayList<String> result) {
+        if (prediction.size() != 6) {
             return false;
         }
-        
-        if(result.size() != 6){
+
+        if (result.size() != 6) {
             return false;
         }
-        
+
         return true;
     }
 
